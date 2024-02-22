275ed0a4eb9e067dca2c2f7f4973b373e02b396d
lucasofaria <lucas092007@hotmail.com>
2024-02-22 00:17:55 -0300

    Login funcionando

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -8,8 +8,11 @@ export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
   const [ user, setUser ] = useState(null);
+  const [ loadingAuth, setLoadingAuth ] = useState(false);
 
   async function signUp(email, password, name){
+    setLoadingAuth(true);
+
     await auth().createUserWithEmailAndPassword(email, password)
     .then( async (value) => {
       let uid = value.user.uid;
@@ -26,17 +29,46 @@ function AuthProvider({ children }){
         }
         
         setUser(data);
+        setLoadingAuth(false);
 
       })
     })
     .catch((error) => {
       console.log(error);
+      setLoadingAuth(false);
       Alert.alert("Atenção", "Erro ao realizar cadastro" );
     })
   }
 
+  async function signIn(email, password){
+    setLoadingAuth(true);
+    
+    await auth().signInWithEmailAndPassword(email, password)
+    .then(async (value) => {
+      let uid = value.user.uid;
+
+      const userProfile = await firestore().collection('users')
+      .doc(uid).get();
+
+      let data = {
+        uid: uid,
+        nome: userProfile.data().nome,
+        email: value.user.email
+      };
+
+      setUser(data);
+      setLoadingAuth(false);
+
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoadingAuth(false);
+      Alert.alert("Atenção", "Erro ao realizar login" );
+    })
+  }
+
   return(
-    <AuthContext.Provider value={{ signed: !!user, signUp }}>
+    <AuthContext.Provider value={{ signed: !!user, signUp, signIn, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from "react";
-import { Text, Alert, Keyboard } from "react-native";
+import { Text, Alert, Keyboard, ActivityIndicator, TouchableWithoutFeedback } from "react-native";
 import { Container, Title, Input, Button, TextButton, AreaText, SignUp } from "./styles";
 
 import { AuthContext } from "../../contexts/auth";
@@ -10,7 +10,7 @@ function Login(){
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
 
-  const { signUp } = useContext(AuthContext);
+  const { signUp, signIn, loadingAuth } = useContext(AuthContext);
 
 
   function toggleLogin(){
@@ -20,10 +20,14 @@ function Login(){
     setName('');
   }
 
-  function handleSignIn(){
+  async function handleSignIn(){
     if(email === '' || password === ''){
       Alert.alert('Atenção', 'Preencha todos os campos!')
     }
+
+    Keyboard.dismiss();
+    await signIn(email, password);
+
   }
 
   async function handleSignUp(){
@@ -31,75 +35,86 @@ function Login(){
       Alert.alert('Atenção', 'Preencha todos os campos para cadastrar!')
     }
 
-    await signUp(email, password, name);
-
     Keyboard.dismiss();
+    await signUp(email, password, name);
 
   }
   
   if(login){
     return(
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <Container>
+          <Title>
+            Dev<Text style={{ color: '#E52246' }}>Connect</Text>
+          </Title>
+    
+          <Input
+            placeholder="digite seu email"
+            onChangeText={(texto) => setEmail(texto)}
+            value={email}
+          />
+    
+          <Input
+            placeholder="digite sua senha"
+            secureTextEntry={true}
+            onChangeText={(texto) => setPassword(texto)}
+            value={password}
+          />
+    
+          <Button onPress={handleSignIn}>
+            {loadingAuth ? (
+              <ActivityIndicator size={20} color={'#FFF'}/>
+            ) : (
+              <TextButton>Acessar</TextButton>
+            )}
+          </Button>
+    
+          <AreaText onPress={toggleLogin}>
+            <SignUp>Criar uma conta</SignUp>
+          </AreaText>
+        </Container>
+      </TouchableWithoutFeedback>
+    )
+  }
+  return(
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <Container>
         <Title>
           Dev<Text style={{ color: '#E52246' }}>Connect</Text>
         </Title>
-  
+
         <Input
-          placeholder="digite seu email"
+          placeholder="Digite seu nome"
+          onChangeText={(texto) => setName(texto)}
+          value={name}
+        />
+
+        <Input
+          placeholder="Digite seu email"
           onChangeText={(texto) => setEmail(texto)}
           value={email}
         />
-  
+
         <Input
-          placeholder="digite sua senha"
+          placeholder="***************"
           secureTextEntry={true}
           onChangeText={(texto) => setPassword(texto)}
           value={password}
         />
-  
-        <Button onPress={handleSignIn}>
-          <TextButton>Acessar</TextButton>
+
+        <Button onPress={handleSignUp}>
+          {loadingAuth ? (
+            <ActivityIndicator size={20} color={'#FFF'}/>
+          ) : (
+            <TextButton>Cadastrar</TextButton>
+          )}
         </Button>
-  
+
         <AreaText onPress={toggleLogin}>
-          <SignUp>Criar uma conta</SignUp>
+          <SignUp>Já tenho uma conta</SignUp>
         </AreaText>
       </Container>
-    )
-  }
-  return(
-    <Container>
-      <Title>
-        Dev<Text style={{ color: '#E52246' }}>Connect</Text>
-      </Title>
-
-      <Input
-        placeholder="Digite seu nome"
-        onChangeText={(texto) => setName(texto)}
-        value={name}
-      />
-
-      <Input
-        placeholder="Digite seu email"
-        onChangeText={(texto) => setEmail(texto)}
-        value={email}
-      />
-
-      <Input
-        placeholder="***************"
-        secureTextEntry={true}
-        onChangeText={(texto) => setPassword(texto)}
-        value={password}
-      />
-
-      <Button onPress={handleSignUp}>
-        <TextButton>Cadastrar</TextButton>
-      </Button>
-
-      <AreaText onPress={toggleLogin}>
-        <SignUp>Já tenho uma conta</SignUp>
-      </AreaText>
-    </Container>
+    </TouchableWithoutFeedback>  
   )
 }
 
