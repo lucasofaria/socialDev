421170065a39177d477ca3da3fb7c0f4c2116531
lucasofaria <lucas092007@hotmail.com>
2024-05-27 23:41:37 -0300

    Adicionar e alterar foto de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,11 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import { Alert, Modal, Platform } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import Header from "../../components/Header";
 import { AuthContext } from "../../contexts/auth";
 import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
+import { launchImageLibrary } from 'react-native-image-picker';
 import { 
   Container,
   Name,
@@ -26,6 +28,21 @@ function Profile(){
   const [nome, setNome] = useState(user?.nome);
   const [url, setUrl] = useState(null);
   const [ open, setOpen ] = useState(false);
+
+  useEffect( () => {
+    async function loadAvatar(){
+      try{
+        let response = await storage().ref('users').child(user?.uid).getDownloadURL();
+        setUrl(response);
+      }catch(err){
+        console.log("Erro ao carregar a foto")
+      }
+    }
+
+    loadAvatar();
+
+    return () => loadAvatar();
+  }, [])
 
   async function handleSignOut(){
     await signOut();
@@ -53,19 +70,77 @@ function Profile(){
     setOpen(false);
   }
 
+  const uploadFile = () => {
+    const options = {
+      noData: true,
+      mediaType: 'photo'
+    };
+
+    launchImageLibrary(options, response => {
+      if(response.didCancel){
+        console.log("Operação cancelada");
+      }else if(response.error){
+        console.log("Ops, algo deu errado!")
+      }else{
+        uploadFileFirebase(response)
+        .then( () => {
+          uploadAvatarPosts();
+        })
+
+        console.log("URI da foto ", response.assets[0].uri);
+        setUrl(response.assets[0].uri)
+      }
+    })      
+  }
+
+  const getFileLocalPath = (response) => {
+    //extrair e retornar a url da foto
+    return response.assets[0].uri;
+  }
+
+  const uploadFileFirebase = async (response) => {
+    const fileSource = getFileLocalPath(response);
+    
+    const storageRef = storage().ref('users').child(user?.uid);
+    
+    return await storageRef.putFile(fileSource)
+  }
+
+  const uploadAvatarPosts = async () => {
+    const storageRef = storage().ref('users').child(user?.uid);
+    const url = await storageRef.getDownloadURL()
+    .then( async (image) => {
+      console.log("URL Recebida ", image)
+
+      // Atualizar as imagens de todos os posts do user
+      const postDocs = await firestore().collection('posts')
+      .where('userId', '==', user.uid).get();
+      
+      // Percorrer todos os posts e atualizar a URL da imagem
+      postDocs.forEach( async doc => {
+        await firestore().collection('posts').doc(doc.id).update({
+          avatarUrl: image
+        })
+      })
+    })
+    .catch( (error) => {
+      console.log("Erro ao atualizar foto do post ", error)
+    })
+  }
+
   return(
     <Container>
       <Header/>
 
       { url ? (
-        <UploadButton>
+        <UploadButton onPress={ () => uploadFile() }>
           <UploadText>+</UploadText>
           <Avatar
             source={{ uri: url }}
           />
         </UploadButton>
       ) : (
-        <UploadButton>
+        <UploadButton onPress={ () => uploadFile() }>
           <UploadText>+</UploadText>
         </UploadButton>
       )}
